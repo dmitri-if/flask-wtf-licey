c5a2a221ae9d6d461cce130aea7eac844b57a272
Dima Nel <asb0rn145@gmail.com>
2025-05-14 01:10:44 +0500

    Двойная защита

--- main.py
+++ main.py
@@ -8,15 +8,9 @@ app.config['SECRET_KEY'] = 'yandexlyceum_secret_key'
 def index():
     return render_template('index.html', title='Заголовок', text1='Миссия Колонизация Марса', text2='И на Марсе будут яблони цвести!')
 
-@app.route('/answer')
-def answer():
-    return render_template('answer.html', title='Watny', surname='Нелюбин', name='Дима', education='Ясли', profession='Отаку', sex='Мужской',
-                           motivation='Люблю аниме', ready='True')
-
-@app.route('/auto_answer')
-def auto_answer():
-    return render_template('auto_answer.html', title='Почти регистрация')
-
+@app.route('/login')
+def login():
+    return render_template('login.html', title='Логин')
 
 if __name__ == '__main__':
     app.run(port=8080, host='127.0.0.1')
