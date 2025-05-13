b222bc0374a744b49014e00ed0bf40e3444628e5
Dima Nel <asb0rn145@gmail.com>
2025-05-13 22:48:26 +0500

    Готовимся к миссии

---
+++ main1.py
@@ -0,0 +1,13 @@
+from flask import Flask, url_for, request, render_template
+import json
+from loginform import *
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = 'yandexlyceum_secret_key'
+
+@app.route('/')
+def index():
+    return render_template('index.html', title='Заголовок', text1='Миссия Колонизация Марса', text2='И на Марсе будут яблони цвести!')
+
+if __name__ == '__main__':
+    app.run(port=8080, host='127.0.0.1')
