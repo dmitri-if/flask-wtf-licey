91f645d66e9bd329af9b9dd035b60e162d195199
Dima Nel <asb0rn145@gmail.com>
2025-05-14 11:01:54 +0500

    По каютам!

--- main.py
+++ main.py
@@ -8,9 +8,11 @@ app.config['SECRET_KEY'] = 'yandexlyceum_secret_key'
 def index():
     return render_template('index.html', title='Заголовок', text1='Миссия Колонизация Марса', text2='И на Марсе будут яблони цвести!')
 
-@app.route('/login')
-def login():
-    return render_template('login.html', title='Логин')
+@app.route('/distribution', methods=['POST'])
+def distribution():
+    items = request.form.getlist('items')
+    return render_template('distribution.html', title='По каютам!', items=items)
+
 
 if __name__ == '__main__':
     app.run(port=8080, host='127.0.0.1')
